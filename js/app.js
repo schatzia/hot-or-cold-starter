7e00c2ec124796080335774fcd496dd572d51cc5
schatzia <schatzia@hotmail.com>
2016-02-12 15:32:48 +0200

    I think it works!

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
 	/*--- Create a new game on page load ---*/
-  
+  hiddenNumber = newGame();
+  countNumber = 0;
 	/*--- Create a new game when the user press the New Game button---*/
 	$('.new').click(function(){
 		hiddenNumber = newGame();
@@ -19,13 +20,18 @@ $(document).ready(function(){
  	});
   	
   /*--- Make a new guess ---*/
-  $('#guessButton').click(function(){
-      var userGuessNumber = parseInt($('#userGuess').val());
-      alert('guess: ' + userGuessNumber + ' hidden: ' + hiddenNumber);
-      var countNumber = parseInt($('#count').text());
-      alert(countNumber);
-      userGuess(userGuessNumber, hiddenNumber);
-      increaseCountNumber(countNumber);
+  $('#guessButton').click(function(event){
+      var userGuessNumber = $('#userGuess').val();
+      event.preventDefault();
+      if($.isNumeric(userGuessNumber)){
+        var countNumber = parseInt($('#count').text());
+        userGuess(userGuessNumber, hiddenNumber);
+        increaseCountNumber(countNumber);
+      }
+      else{
+        $('#userGuess').val('');
+        alert("Please input a valid number!");
+      }
   }); 
 });
 
@@ -33,36 +39,40 @@ $(document).ready(function(){
 
 function newGame(){
       $('#userGuess').val('');
+      $('#feedback').text('Make your Guess!');
+      $('#count').text('0');
+      $('li.guess').remove();
+      $('#guessButton').show();
       var secretNumber = Math.floor((Math.random()*100) +1);
       return secretNumber;
     }
 
 function userGuess(guessNumber, hiddenNumber){
       var difference = Math.abs(guessNumber - hiddenNumber);
-      alert(difference);
 
       if (difference == 0){
         $('#feedback').text('You Won. Click new game to play again');
+        $('#guessButton').hide();
       }
       else if (difference >=50){
         $('#feedback').text('Ice cold');
-        $('#guessList').append('<li>' + guessNumber + '</li>');       
+        $('#guessList').append('<li class="guess">' + guessNumber + '</li>');       
       }
       else if (difference >=30 && difference < 50){
         $('#feedback').text('cold');
-        $('#guessList').append('<li>' + guessNumber + '</li>');
+        $('#guessList').append('<li class="guess">' + guessNumber + '</li>');
       }
       else if (difference >=20 && difference < 30){
         $('#feedback').text('warm');
-        $('#guessList').append('<li>' + guessNumber + '</li>');
+        $('#guessList').append('<li class="guess">' + guessNumber + '</li>');
       }
       else if (difference >=10 && difference < 20){
         $('#feedback').text('hot');
-        $('#guessList').append('<li>' + guessNumber + '</li>');
+        $('#guessList').append('<li class="guess">' + guessNumber + '</li>');
       }
       else if (difference >=1 && difference < 10){
         $('#feedback').text('very hot');
-        $('#guessList').append('<li>' + guessNumber + '</li>');
+        $('#guessList').append('<li class="guess">' + guessNumber + '</li>');
       }
     }
 function increaseCountNumber(countNumber){
